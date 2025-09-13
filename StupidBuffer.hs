7dae745369528d026194b11a9daf2c96d5d174aa
Bodigrim <andrew.lelechenko@gmail.com>
2025-09-13 08:35:26 +0200

    Draft

--- StupidBuffer.hs
+++ StupidBuffer.hs
@@ -8,19 +8,35 @@ module Lib where
 import Control.Monad.ST
 import Data.Primitive.ByteArray
 import Data.Text.Internal
+import Prelude hiding ((++))
 
-data Builder = Builder ByteArray Int
+data Buffer = Buffer ByteArray Int
 
-append :: Builder -> Text -> Builder
-append (Builder arr used) (Text srcArr srcOff srcLen) = runST $ do
+(++) :: Buffer -> Text -> Buffer
+Buffer arr used ++ Text srcArr srcOff srcLen = runST $ do
   let unused = sizeofByteArray arr - used
   if unused >= srcLen then do
     mutArr <- unsafeThawByteArray arr
+    copyByteArray mutArr used srcArr srcOff srcLen
     arr' <- unsafeFreezeByteArray mutArr
-    pure $ Builder arr' (used + srcLen)
+    pure $ Buffer arr' (used + srcLen)
   else do
     mutArr <- newByteArray ((used + srcLen) * 2)
     copyByteArray mutArr 0 arr 0 used
     copyByteArray mutArr used srcArr srcOff srcLen
     arr' <- unsafeFreezeByteArray mutArr
-    pure $ Builder arr' (used + srcLen)
+    pure $ Buffer arr' (used + srcLen)
+
+data MutableBuffer s = MutableBuffer (MutableByteArray s) Int
+
+(+++) :: MutableBuffer s -> Text -> ST s (MutableBuffer s)
+MutableBuffer mutArr used +++ Text srcArr srcOff srcLen = do
+  size <- getSizeofMutableByteArray mutArr
+  let unused = size - used
+  if unused >= srcLen then do
+    copyByteArray mutArr used srcArr srcOff srcLen
+    pure $ MutableBuffer mutArr (used + srcLen)
+  else do
+    mutArr' <- resizeMutableByteArray mutArr ((used + srcLen) * 2)
+    copyByteArray mutArr' used srcArr srcOff srcLen
+    pure $ MutableBuffer mutArr' (used + srcLen)
